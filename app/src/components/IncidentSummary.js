8fa3938fc5a2a2ce0a58754b94521f9fd6b3b487
Tara Larsen <talarsen74@gmail.com>
2021-11-04 17:44:56 -0700

    add more IncidentSummary cards to page and styled them. Still need to fix tcurrentevacuations

--- app/src/components/IncidentSummary.js
+++ app/src/components/IncidentSummary.js
@@ -66,6 +66,46 @@ const IncidentSummary = ({ incidentNumber }) => {
             <h4>{incident.fatalities}</h4>
           </div>
         </div>
+        <div className={styles.card}>
+          <div className={styles.cardIcon}>
+            {" "}
+            <Icon icon="gis:layer-landcover" />{" "}
+          </div>
+          <div className={styles.cardTitle}>
+            <h5>Acreage</h5>
+            <h4>{incident.acreage}</h4>
+          </div>
+        </div>
+        <div className={styles.card}>
+          <div className={styles.cardIcon}>
+            {" "}
+            <Icon icon="ic:outline-percent" />{" "}
+          </div>
+          <div className={styles.cardTitle}>
+            <h5>Percent Contained</h5>
+            <h4>{incident.percent_contained}</h4>
+          </div>
+        </div>
+        <div className={styles.card}>
+          <div className={styles.cardIcon}>
+            {" "}
+            <Icon icon="ic:sharp-night-shelter" />{" "}
+          </div>
+          <div className={styles.cardTitle}>
+            <h5>Current Evacuations</h5>
+            <h4>{incident.current_evacuations}</h4>
+          </div>
+        </div>
+        <div className={styles.cardFuels}>
+          <div className={styles.cardIcon}>
+            {" "}
+            <Icon icon="tabler:trees" />{" "}
+          </div>
+          <div className={styles.cardTitle}>
+            <h5>Fuels Involved</h5>
+            <h4>{incident.fuels_involved}</h4>
+          </div>
+        </div>
       </div>
     </>
   );
